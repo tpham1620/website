7806c08d1e320b89a6f88b7b6d65d92ec175e7fd
Tan Pham <ngtan1620@gmail.com>
2017-06-03 16:59:42 -0700

    update service menus store data on database server instead of inside html files

---
+++ php/service_get_wax.php
@@ -0,0 +1,24 @@
+<?php
+	$dsn = 'mysql:host=tphaminstance.cocmkr81a0rx.us-west-2.rds.amazonaws.com;dbname=jcnailsspa';
+    $username = 'tpham1620';
+    $password = 'NewChapter2016';
+
+try {
+		$conn = new PDO($dsn, $username, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$sql = "SELECT * FROM jcnailsspa.service_wax";
+		$query = $conn->query($sql);
+		$des_out = $query->fetchAll(PDO::FETCH_ASSOC);
+		if ($des_out) {	
+			echo json_encode($des_out);
+		}
+	}
+catch(PDOException $e)
+    {
+     echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
